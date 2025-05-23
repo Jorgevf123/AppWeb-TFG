5adc28462118333c9255b35dc165cdcdc984b836
Jorge Ángel Vázquez <jorgeangel.vazquezfernandez@usp.ceu.es>
2025-05-23 21:45:57 +0200

    Cambios en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
+const isRender = process.env.RENDER === "true";
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
-    host: "::",
+    host: true,
     port: 8080,
     allowedHosts: ['appweb-tfg-frontend.onrender.com'],
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: isRender
+          ? "https://appweb-tfg.onrender.com" 
+          : "http://localhost:5000", 
         changeOrigin: true,
       },
     },
