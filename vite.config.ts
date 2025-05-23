09362bd95d52f59ccefdf5e857582093068b2859
Jorge Ángel Vázquez <jorgeangel.vazquezfernandez@usp.ceu.es>
2025-05-23 21:09:26 +0200

    Añadida linea a vite.config.ts para que acepte el dominio de render

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['appweb-tfg-frontend.onrender.com'],
     proxy: {
       "/api": {
         target: "http://localhost:5000",
