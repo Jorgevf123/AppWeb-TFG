fcda89441aae6d14022592e957befed8191fda5a
Jorge Ángel Vázquez <jorgeangel.vazquezfernandez@usp.ceu.es>
2025-05-23 18:39:45 +0200

    Cambiado el nombre de middleware/admin a middleware/Admin

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,4 +1,3 @@
-// server/middleware/auth.js
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -3,11 +3,9 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (_req, _file, cb) {
-    // Ruta absoluta a la carpeta uploads (dentro de server/uploads)
     cb(null, path.join(__dirname, "../uploads"));
   },
   filename: function (_req, file, cb) {
-    // Nombre único para evitar sobrescribir
     const uniqueName = `${Date.now()}-${file.originalname}`;
     cb(null, uniqueName);
   }
