bd94590f143869655425cd0c6d58499716433681
Jorge Ángel Vázquez <jorgeangel.vazquezfernandez@usp.ceu.es>
2025-06-01 18:14:27 +0200

    Nuevo cambio en cors de index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,13 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS no permitido: " + origin));
+    }
+  },
   credentials: true
 }));
 
